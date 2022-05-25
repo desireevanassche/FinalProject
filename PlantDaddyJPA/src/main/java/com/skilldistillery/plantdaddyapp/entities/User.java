f9fb4d0f8f79d21aab02da18b31300ef191f8fa3
Lpaladini90 <lpaladini@me.com>
2022-05-25 09:04:26 -0700

    Checking entities

--- PlantDaddyJPA/src/main/java/com/skilldistillery/plantdaddyapp/entities/User.java
+++ PlantDaddyJPA/src/main/java/com/skilldistillery/plantdaddyapp/entities/User.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -64,7 +63,6 @@ public class User {
 	@JoinColumn(name="address_id")
 	private Address address;
 		
-	
 	@OneToMany(mappedBy="user")
 	private List<Blog> blogs;
 	
