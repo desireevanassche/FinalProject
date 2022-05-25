7b33fecbabe04c702c51305eaa8f0ca093268f2a
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-05-25 08:24:38 -0600

    hashtag entity created

---
+++ PlantDaddyJPA/src/main/java/com/skilldistillery/plantdaddyapp/entities/Hashtag.java
@@ -0,0 +1,61 @@
+package com.skilldistillery.plantdaddyapp.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hashtag {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	public Hashtag() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Hashtag [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hashtag other = (Hashtag) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
