205296baeadc4b2f8fa4b79ca699555afae7380a
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-05-26 15:23:47 -0600

    fixed friend address id in user table

--- PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/controllers/PlantController.java
+++ PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/controllers/PlantController.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,18 +30,30 @@ public class PlantController {
 	public List<Plant> index() {
 		return plantServ.index();
 	}
-	
+
 	@GetMapping
 	public List<Plant> indexByUser(Principal principal, HttpServletResponse res) {
 		return plantServ.indexByUsername(principal.getName());
 	}
+
 	@PostMapping
 	public Plant createPlant(@RequestBody Plant plant, HttpServletResponse res, Principal pricipal) {
 		Plant newPlant = plantServ.addPlant(plant, pricipal.getName());
-		if(newPlant != null) {
+		if (newPlant != null) {
 			res.setStatus(201);
 		}
 		return newPlant;
+	}
+
+	@PutMapping
+	public Plant updatePlant(@PathVariable int id, @RequestBody Plant plant, HttpServletResponse res, Principal principal ) {
+		
+		
+		
+		
+		
+		return plant;
+		
 	}
 
 }
