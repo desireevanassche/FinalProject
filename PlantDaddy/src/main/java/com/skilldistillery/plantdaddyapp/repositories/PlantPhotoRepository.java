5ceaf143fc9234edbe5879ac601aa5054e6c8197
Lpaladini90 <lpaladini@me.com>
2022-05-26 19:36:47 -0700

    plant photo stubs created in serv, impl and rep

---
+++ PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/repositories/PlantPhotoRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.plantdaddyapp.repositories;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.skilldistillery.plantdaddyapp.entities.PlantPhoto;
+
+public interface PlantPhotoRepository extends JpaRepositoryImplementation<PlantPhoto, Integer>{
+
+}
