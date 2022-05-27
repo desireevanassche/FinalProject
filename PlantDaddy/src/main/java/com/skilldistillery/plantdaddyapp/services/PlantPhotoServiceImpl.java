c61eb3aa9417b74928f7f753ff67c224a0ef02b1
Lpaladini90 <lpaladini@me.com>
2022-05-26 19:48:33 -0700

    service and impl created for index of all photos

--- PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/services/PlantPhotoServiceImpl.java
+++ PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/services/PlantPhotoServiceImpl.java
@@ -1,10 +1,32 @@
 package com.skilldistillery.plantdaddyapp.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.plantdaddyapp.entities.PlantPhoto;
+import com.skilldistillery.plantdaddyapp.repositories.PlantPhotoRepository;
 
 
 @Service
 public class PlantPhotoServiceImpl implements PlantPhotoServices {
+	
+	
+	@Autowired
+	private PlantPhotoRepository photoRepo;
+
+	@Override
+	public List<PlantPhoto> index() {
+		
+		return photoRepo.findAll();
+	}
+
+	@Override
+	public List<PlantPhoto> showByUserPlantId(int userPlantId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 }
--- PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/services/PlantPhotoServices.java
+++ PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/services/PlantPhotoServices.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.plantdaddyapp.services;
 
+import java.util.List;
+
+import com.skilldistillery.plantdaddyapp.entities.PlantPhoto;
+
 public interface PlantPhotoServices {
 
+	public List<PlantPhoto> index();
+	
+	public List<PlantPhoto> showByUserPlantId(int userPlantId);
+	
+	
+	
+	
+	
+	
+	
+	
 }
