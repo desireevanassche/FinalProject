c73242ec7a07aa05c520b5123077abb48f42b6a4
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-05-26 18:04:23 -0600

    creating service/controller for userplant

--- PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/controllers/UserPlantController.java
+++ PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/controllers/UserPlantController.java
@@ -1,5 +1,61 @@
 package com.skilldistillery.plantdaddyapp.controllers;
 
+import java.security.Principal;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.plantdaddyapp.entities.UserPlant;
+import com.skilldistillery.plantdaddyapp.services.UserPlantService;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin({ "*", "http://localhost:4295" })
 public class UserPlantController {
+
+	@Autowired
+	UserPlantService userPlantServ;
+
+	@GetMapping("userPlants")
+	public List<UserPlant> index(Principal principal) {
+		return userPlantServ.index(principal.getName());
+	}
+
+	@GetMapping("userPlants{userPlantId}")
+	public UserPlant show(HttpServletResponse res, @PathVariable int userPlantId, Principal principal) {
+		UserPlant userPlant = userPlantServ.show(principal.getName(), userPlantId);
+		if (userPlant == null) {
+			res.setStatus(404);
+		}
+		return userPlant;
+	}
+
+	@PostMapping("userplants")
+	public UserPlant addUserPlant(@RequestBody UserPlant userPlant, HttpServletResponse res, Principal principal) {
+		UserPlant newUserPlant = userPlantServ.addPlant(userPlant, principal.getName());
+		return newUserPlant;
+	}
+
+	@PutMapping("userplants/{userPlantId}")
+	public UserPlant updateUserPlant(@RequestBody UserPlant userPlant, @PathVariable int userPlantId,
+			Principal principal) {
+		return userPlantServ.updatePlant(userPlant, userPlantId, principal.getName());
+	}
+
+	@PutMapping("userplants/{userPlantId}")
+	public UserPlant deactivate(@RequestBody UserPlant userPlant, HttpServletResponse res, Principal principal) {
+		return userPlant;
+
+	}
 
 }
