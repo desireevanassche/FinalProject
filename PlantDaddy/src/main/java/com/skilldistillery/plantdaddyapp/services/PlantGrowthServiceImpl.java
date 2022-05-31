bc8b7b2ef4f9bbaa56bae97ab8968b41a718e57a
Lpaladini90 <lpaladini@me.com>
2022-05-31 14:57:19 -0700

    delete growth data entry made

--- PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/services/PlantGrowthServiceImpl.java
+++ PlantDaddy/src/main/java/com/skilldistillery/plantdaddyapp/services/PlantGrowthServiceImpl.java
@@ -16,91 +16,103 @@ import com.skilldistillery.plantdaddyapp.repositories.UserRepository;
 @Service
 public class PlantGrowthServiceImpl implements PlantGrowthService {
 
-	
 	@Autowired
 	private PlantGrowthRepository growRepo;
 
 	@Autowired
 	private UserPlantRepository plantRepo;
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Override
 	public List<PlantGrowth> indexGrowthByUserPlantId(int userPlantId, String username) {
 		System.out.println(userPlantId);
-	User user = userRepo.findByUsername(username);
-	
-	if(user!=null) {
-	 Optional<UserPlant> plantOp = plantRepo.findById(userPlantId);
-	 
-	 
-	 if(plantOp.isPresent()) {
-		 UserPlant plant = plantOp.get();
-		 List<PlantGrowth> growth = growRepo.findByUserPlant_Id(plant.getId());
-		 
-		 return growth;
-	 }
-	}
+		User user = userRepo.findByUsername(username);
+
+		if (user != null) {
+			Optional<UserPlant> plantOp = plantRepo.findById(userPlantId);
+
+			if (plantOp.isPresent()) {
+				UserPlant plant = plantOp.get();
+				List<PlantGrowth> growth = growRepo.findByUserPlant_Id(plant.getId());
+
+				return growth;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public PlantGrowth findGrowthById(int userPlantId, int growthId, String username) {
 		User user = userRepo.findByUsername(username);
-		
-		if(user!=null) {
-			
-			 Optional<UserPlant> plantOp = plantRepo.findById(userPlantId);
-			 
-			 if(plantOp.isPresent()) {
-				 
-				 UserPlant plant = plantOp.get();
-				 plant.setUser(user);
-				 Optional<PlantGrowth> op = growRepo.findById(growthId);
-				 
-				 if(op.isPresent()) {
-					 PlantGrowth growth = op.get();
-					 growth.setUserPlant(plant);
-				
-				 
-				 return growth;
-				 }
-			 }
-			 
+
+		if (user != null) {
+
+			Optional<UserPlant> plantOp = plantRepo.findById(userPlantId);
+
+			if (plantOp.isPresent()) {
+
+				UserPlant plant = plantOp.get();
+				plant.setUser(user);
+				Optional<PlantGrowth> op = growRepo.findById(growthId);
+
+				if (op.isPresent()) {
+					PlantGrowth growth = op.get();
+					growth.setUserPlant(plant);
+
+					return growth;
+				}
 			}
-		
+
+		}
+
 		return null;
 	}
 
 	@Override
 	public PlantGrowth addGrowth(int userPlantId, String username, PlantGrowth growth) {
-		
-User user = userRepo.findByUsername(username);
-		
-		if(user!=null) {
-			
-			 Optional<UserPlant> plantOp = plantRepo.findById(userPlantId);
-			 
-			 if(plantOp.isPresent()) {
-				 UserPlant plant = plantOp.get();
-				 plant.setUser(user);
-				 growth.setUserPlant(plant);
-				 
-			 }
-		}
-		return growRepo.saveAndFlush(growth); 
-	}
 
+		User user = userRepo.findByUsername(username);
+
+		if (user != null) {
+
+			Optional<UserPlant> plantOp = plantRepo.findById(userPlantId);
+
+			if (plantOp.isPresent()) {
+				UserPlant plant = plantOp.get();
+				plant.setUser(user);
+				growth.setUserPlant(plant);
+
+			}
+		}
+		return growRepo.saveAndFlush(growth);
+	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 	@Override
 	public boolean deleteGrowth(int growthId, int userPlantId, String username) {
-		
-		
-		return false;
+		boolean deleted = false;
+
+		PlantGrowth existing = growRepo.findByUser_UsernameAndId(username, growthId);
+
+		if (existing != null) {
+			growRepo.delete(existing);
+			deleted = true;
+
+		}
+
+		return deleted;
 	}
-	
-	
-	
-	
+
 }
